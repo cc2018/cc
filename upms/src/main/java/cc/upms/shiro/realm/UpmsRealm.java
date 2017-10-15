ec9aaa6d2150e9724897117697892b21b141d041
caojian02 <caojian02@baidu.com>
2017-10-15 23:04:03 +0800

    shiro

---
+++ upms/src/main/java/cc/upms/shiro/realm/UpmsRealm.java
@@ -0,0 +1,95 @@
+package cc.upms.shiro.realm;
+
+import cc.upms.entitys.UserInfo;
+import cc.upms.service.api.UserInfoService;
+import cc.upms.util.MD5Util;
+import cc.upms.util.PropertiesFileUtil;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UpmsRealm extends AuthorizingRealm {
+
+    private static Logger _log = LoggerFactory.getLogger(UpmsRealm.class);
+
+    @Autowired
+    private UserInfoService userInfoService;
+
+    /**
+     * 授权：验证权限时调用
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        String username = (String) principalCollection.getPrimaryPrincipal();
+        /*UpmsUser upmsUser = upmsApiService.selectUpmsUserByUsername(username);
+
+        // 当前用户所有角色
+        List<UpmsRole> upmsRoles = upmsApiService.selectUpmsRoleByUpmsUserId(upmsUser.getUserId());
+        Set<String> roles = new HashSet<>();
+        for (UpmsRole upmsRole : upmsRoles) {
+            if (StringUtils.isNotBlank(upmsRole.getName())) {
+                roles.add(upmsRole.getName());
+            }
+        }
+
+        // 当前用户所有权限
+        List<UpmsPermission> upmsPermissions = upmsApiService.selectUpmsPermissionByUpmsUserId(upmsUser.getUserId());
+        Set<String> permissions = new HashSet<>();
+        for (UpmsPermission upmsPermission : upmsPermissions) {
+            if (StringUtils.isNotBlank(upmsPermission.getPermissionValue())) {
+                permissions.add(upmsPermission.getPermissionValue());
+            }
+        }*/
+
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        Set<String> roles = new HashSet<>();
+        Set<String> permissions = new HashSet<>();
+        simpleAuthorizationInfo.setStringPermissions(permissions);
+        simpleAuthorizationInfo.setRoles(roles);
+        return simpleAuthorizationInfo;
+    }
+
+    /**
+     * 认证：登录时调用
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String) authenticationToken.getPrincipal();
+        String password = new String((char[]) authenticationToken.getCredentials());
+        // client无密认证
+        String upmsType = PropertiesFileUtil.getInstance("upms").get("upms.type");
+        if ("client".equals(upmsType)) {
+            return new SimpleAuthenticationInfo(username, password, getName());
+        }
+
+        // 查询用户信息
+        UserInfo upmsUser = userInfoService.findByUserName(username);
+
+        if (null == upmsUser) {
+            throw new UnknownAccountException();
+        }
+        if (!upmsUser.getPassword().equals(MD5Util.MD5(password + upmsUser.getSalt()))) {
+            throw new IncorrectCredentialsException();
+        }
+        if (upmsUser.getLocked() == 1) {
+            throw new LockedAccountException();
+        }
+
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+
+}
